1f9efa91630ea71ab9d03de606831e4d8c3f0c87
mepranavpatil <mepranavpatil@gmail.com>
2026-08-13 19:27:29 +0530

    .

--- main.tf
+++ main.tf
@@ -7,9 +7,65 @@ resource "aws_vpc" "test1vpc" {
 }
 resource "aws_subnet" "test1subnet" {
   vpc_id     = aws_vpc.test1vpc.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_cidr
   availability_zone = "ap-south-1a"
   tags = {
     Name = "test1subnet"
   }
+}
+resource "aws_route" "test1route" {
+  route_table_id         = aws_route_table.test1rt.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+resource "aws_internet_gateway" "test1igw" {
+  vpc_id = aws_vpc.test1vpc.id
+  tags = {
+    Name = "test1igw"
+  }
+  
+}
+resource "aws_route_table" "test1rt" {
+  vpc_id = aws_vpc.test1vpc.id
+  route {
+    cidr_block = var.vpc_cidr
+    gateway_id = aws_internet_gateway.test1igw.id
+  }
+  tags = {
+    Name = "test1rt"
+  }
+}
+resource "aws_route_table_association" "test1rta" {
+  subnet_id      = aws_subnet.test1subnet.id
+  route_table_id = aws_route_table.test1rt.id
+}
+resource "aws_security_group" "test1sg" {
+  name        = "test1sg"
+  description = "Security group for test1"
+  vpc_id      = aws_vpc.test1vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_instance" "test1instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.test1subnet.id
+  security_groups = [aws_security_group.test1sg.id]
+  key_name = var.key-pair
 }
--- variable.tf
+++ variable.tf
@@ -1,7 +1,35 @@
+#region
+variable "regions" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+  default     = "ap-south-1"
+}
+
 #vpc
 variable "vpc_cidr" {
   description = "The CIDR block for the VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
-
+#subnet
+variable "subnet_cidr" {
+  description = "The CIDR block for the subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+#instance
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro"
+}
+variable "ami_id" {
+  description = "The AMI ID to use for the instance"
+  type        = string
+  default     = "ami-0c55b159cbfafe1d0"
+}
+variable "key-pair" {
+  description = "The name of the key pair to use for the instance"
+  type        = string
+  default     = "my-key-pair"
+}
